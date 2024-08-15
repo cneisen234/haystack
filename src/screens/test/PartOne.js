e60268d4207c81a1bcf98dd3068a43023ddfa4dc
Christopher Neisen <christopherjay71186@gmail.com>
2024-08-15 13:36:19 -0500

    first task done

--- src/screens/test/PartOne.js
+++ src/screens/test/PartOne.js
@@ -1,11 +1,33 @@
-import { SafeAreaView, Text } from "react-native";
+import React from 'react';
+import { SafeAreaView, FlatList, Text, View, StyleSheet } from 'react-native';
+import localPlaceholderData from '../../localPlaceholderData';
 
-export default function () {
-    return (
-        <SafeAreaView>
-            <Text>
-                Part One
-            </Text>
-        </SafeAreaView>
-    )
-}
+const PartOne = () => {
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text>{item.id}</Text>
+      <Text>{item.title}</Text>
+    </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={localPlaceholderData}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  item: {
+    padding: 10,
+  },
+});
+
+export default PartOne;
